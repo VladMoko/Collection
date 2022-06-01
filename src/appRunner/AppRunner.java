8ec92d9b54c28bf50a478186c30f7aa204258db9
VladMoko <advmoney2233@gmail.com>
2022-06-01 18:16:32 +0300

    Changes

--- src/appRunner/AppRunner.java
+++ src/appRunner/AppRunner.java
@@ -1,11 +1,39 @@
 package appRunner;
 
-import collection.Collection;
+import circle.Circle;
+import shape.Shape;
+import square.Square;
+import triangle.Triangle;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
 
 public class AppRunner {
 
+    public static final Logger LOGGER = Logger.getLogger(AppRunner.class.getName());
+
     public static void main(String[] args) {
-        Collection figure = new Collection();
-        figure.getArea();
+        //Makes new collection
+        ArrayList<Shape> shapes = new ArrayList<>();
+        shapes.add(new Square(2));
+        shapes.add(new Circle(5));
+        shapes.add(new Triangle(5, 2));
+
+        //Set index to zero
+        Shape min = shapes.get(0);
+        Shape max = shapes.get(0);
+
+        //Search min & max element
+        for (Shape i: shapes) {
+            if (min.getArea() > i.getArea()){
+                min = i;
+            }
+            if (max.getArea() < i.getArea()){
+                max = i;
+            }
+        }
+        //Writing info about our elements
+        LOGGER.info("Min area is " + min.getArea() + ", shape is " + min.getClass().getSimpleName());
+        LOGGER.info("max area is " + max.getArea() + ", shape is " + max.getClass().getSimpleName());
     }
 }
